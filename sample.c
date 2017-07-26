d6229ea8b96e0ade772917b1fcbf7b428a95556a
Dmitry <dimonija@mail.ru>
2017-07-26 16:34:57 +0300

    Cosmetic fix in sample

--- sample.c
+++ sample.c
@@ -1,11 +1,27 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <httpq.h>
 
 #define URL_MAX_LEN 256
 
 int main(int argc, char** argv)
 {
+    long http, res;
+    char url[URL_MAX_LEN];
+    char *response;
+    const char *post_data[][2] = {
+        {"parse_mode", "HTML"},
+        {"chat_id", argv[2]},
+        {"text", "<b>Hi!</b>\n<code>This is httpq sample message: 0123456789. Some special characters: [@][&][=][?]</code>"},
+        {"dummy", "dummy"},
+        {NULL, NULL}
+    };
+    const char *header_data[] = {
+        "Accept: application/json",
+        "Accept-Language: en_US",
+        NULL
+    };
+
     printf("libHTTPQ sample\n");
 
     if (argc != 3)
@@ -14,22 +30,6 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    char url[URL_MAX_LEN];
-
-    const char *post_data[][2] = {
-        {"parse_mode", "HTML"},
-        {"chat_id", argv[2]},
-        {"text", "<b>Hi!</b>\n<code>This is httpq sample message: 0123456789. Some special characters: [@][&][=][?]</code>"},
-        {"dummy", "dummy"},
-        {NULL, NULL}
-    };
-
-    const char *header_data[] = {
-        "Accept: application/json",
-        "Accept-Language: en_US",
-        NULL
-    };
-
     snprintf(url, URL_MAX_LEN, "https://api.telegram.org/bot%s/sendMessage", argv[1]);
 
     httpq_init();
@@ -37,12 +37,11 @@ int main(int argc, char** argv)
     httpq_set_post(post_data);
     httpq_set_headers(header_data);
 
-    long http, res;
-    char *resp = httpq_request_post(&res, &http);
-    if (resp)
+    response = httpq_request_post(&res, &http);
+    if (res == 0)
     {
-        printf("resp[%s]\n", resp);
-        free(resp);
+        printf("Response[%s]\n", response);
+        free(response);
     }
     printf("libHTTPQ sample result[%ld] http code[%ld] error string[%s]\n", res, http, httpq_error(res));
 
